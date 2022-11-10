0d3a5d1534dc3d8ac4791aec91ff4f2ff9c3a26b
Leandro Rosso <lrosso.ti@gmail.com>
2022-11-10 18:56:25 -0300

    Commit 10-11-2022

--- app/controllers/api/v1/contas_controller.rb
+++ app/controllers/api/v1/contas_controller.rb
@@ -1,51 +1,52 @@
-module Api
-  module V1
-    class ContasController < ApplicationController
-      before_action :set_conta, only: [:show, :update, :destroy]
+class Api::V1::ContasController < ApplicationController
+  before_action :set_conta, only: [:show, :update, :destroy]
 
-      def index
-        render json: Conta.all.to_json
-      end
+  def index
+    render json: Conta.all.to_json
+  end
 
-      def show
-        render json: @conta
-      end
+  def show
+    render json: @conta
+  end
 
-      def create
-        @conta = Conta.new(conta_params)
-        if @conta.save
-          render json: { successful: true }
-        else
-          render json: @conta.errors, status: :unprocessable_entity
-        end
-      end
-
-      def update
-        if @conta.update(conta_params)
-          render json: @conta
-        else
-          render json: @conta.errors, status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /api/v1/contas/1
-      def destroy
-
-        if @conta.destroy
-          render json: { successful: true }
-        else
-          render json: @conta.errors, status: :unprocessable_entity
-        end
-      end
-
-      private
-      def set_conta
-        @conta = Conta.find(params[:id])
-      end
-
-      def conta_params
-        params.permit(:titulo, :saldo)
-      end
+  def create
+    @conta = Conta.new(conta_params)
+    if @conta.save
+      render json: { message: 'Conta criada com sucesso' }, status: 201
+    else
+      # render json: @conta.errors, status: :unprocessable_entity
+      render json: { error: 'Não foi possível salvar a Conta' }, status: 400
     end
+  end
+
+  def update
+    if @conta.update(conta_params)
+      render json: { message: 'Conta alterada com sucesso' }, status: 200
+    else
+      # render json: @conta.errors, status: :unprocessable_entity
+      render json: { error: 'Não foi possível alterar a Conta' }, status: 400
+    end
+  end
+
+  # DELETE /api/v1/contas/1
+  def destroy
+
+    if @conta.destroy
+      # render json: { successful: true }
+      render json: { message: 'Conta excluida com sucesso' }, status: 204
+    else
+      # render json: @conta.errors, status: :unprocessable_entity
+      render json: { error: 'Não foi possível excluir a Conta' }, status: 400
+    end
+  end
+
+  private
+
+  def set_conta
+    @conta = Conta.find(params[:id])
+  end
+
+  def conta_params
+    params.permit(:titulo, :saldo)
   end
 end
--- app/controllers/api/v1/transacaos_controller.rb
+++ app/controllers/api/v1/transacaos_controller.rb
@@ -1,47 +1,51 @@
-module Api
-  module V1
-    class TransacaosController < ApplicationController
-      before_action :set_transacao, only: [:show, :update, :destroy]
+class Api::V1::TransacaosController < ApplicationController
+  before_action :set_transacao, only: [:show, :update, :destroy]
 
-      def index
-        render json: Transacao.all.to_json
-      end
+  def index
+    render json: Transacao.all.to_json
+  end
 
-      def show
-        render json: @transacao
-      end
+  def show
+    render json: @transacao
+  end
 
-      def create
-        @transacao = Transacao.new(transacao_params)
-        if @transacao.save
-          render json: { successful: true }
-        else
-          render json: @transacao.errors, status: :unprocessable_entity
-        end
-      end
+  def create
+    @transacao = Transacao.new(transacao_params)
 
-      def update
-        if @transacao.update(transacao_params)
-          render json: @transacao
-        else
-          render json: @transacao.errors, status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /api/v1/transacao/1
-      def destroy
-        @transacao.destroy
-      end
-
-      private
-
-      def set_transacao
-        @transacao = Transacao.find(params[:id])
-      end
-
-      def transacao_params
-        params.permit(:tipo, :titulo, :descricao, :valor, :data, :conta_id)
-      end
+    if @transacao.save
+      render json: { message: 'Transação criada com sucesso' }, status: 201
+    else
+      # render json: @transacao.errors, status: :unprocessable_entity
+      render json: { error: 'Não foi possível salvar a Transação' }, status: 400
     end
+  end
+
+  def update
+    if @transacao.update(transacao_params)
+      render json: { message: 'Transação alterada com sucesso' }, status: 200
+    else
+      # render json: @transacao.errors, status: :unprocessable_entity
+      render json: { error: 'Não foi possível alterar a Transação' }, status: 400
+    end
+  end
+
+  # DELETE /api/v1/transacao/1
+  def destroy
+    if @transacao.destroy
+      render json: { message: 'Transação excluida com sucesso' }, status: 204
+    else
+      # render json: @conta.errors, status: :unprocessable_entity
+      render json: { error: 'Não foi possível excluir a Transação' }, status: 400
+    end
+  end
+
+  private
+
+  def set_transacao
+    @transacao = Transacao.find(params[:id])
+  end
+
+  def transacao_params
+    params.permit(:tipo, :titulo, :descricao, :valor, :data, :conta_id)
   end
 end
